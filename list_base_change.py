9073bbdb34ad4ca1a0106ead8fa0620ca8797fdf
Justfastfail <justfastfail@gmail.com>
2017-10-15 19:03:36 +0200

    Añadido el list_base_change que cambia a una base cualquiera en modo lista cualquier decimal

---
+++ list_base_change.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+# nelementos is the number of elements for the binary
+def list_base_change(decimal, base, nelementos):
+
+    binario = []
+    base = 4
+
+    while decimal // base != 0:
+        binario = [decimal % base] + binario
+        decimal = decimal // base
+    binario = [decimal] + binario
+    binario = [0] * (nelementos - len(binario)) + binario
+    return binario
+
+# print(list_base_change(7, 2, 8))
